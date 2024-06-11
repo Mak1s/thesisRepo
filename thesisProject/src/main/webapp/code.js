06cfd07efca25ab5ea7490265c8befd27711d708
Makis <gerrylog@yahoo.com>
2024-06-11 20:04:03 +0300

    added getX3ML function

--- thesisProject/src/main/webapp/code.js
+++ thesisProject/src/main/webapp/code.js
@@ -56,28 +56,35 @@ function logout() {
     window.location.href = 'login.html'; 
 }
 
-document.getElementById('uploadForm').addEventListener('submit', async function (event) {
-    event.preventDefault();
-    
-    const formData = new FormData();
+function getX3MLFile(){
+    let myForm = document.getElementById('uploadForm');
+    let formData = new FormData(myForm);
     const fileInput = document.getElementById('fileUpload');
     const file = fileInput.files[0];
-    
-    formData.append('fileUpload', file);
-  
-    try {
-      const response = await fetch('/upload', {
-        method: 'POST',
-        body: formData
-      });
-      
-      if (response.ok) {
-        const result = await response.json();
-        console.log('File uploaded successfully', result);
-      } else {
-        console.error('Error uploading file');
-      }
-    } catch (error) {
-      console.error('Error:', error);
+    if(!file){
+        document.getElementById('addedContent').innerHTML="Error";
+        return;
     }
-  });
+    var xhr = new XMLHttpRequest();
+
+    xhr.onload = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const responseData = JSON.parse(xhr.responseText);
+            $('#addedContent').html("File uploaded successfully!<br> File Data");
+            $('#addedContent').append(createTableFromJSON(responseData));
+            console.log(responseData);
+        } else if (xhr.status !== 200) {
+            document.getElementById("addedContent").innerHTML = "Request failed. Returned status of " + xhr.status + "<br>";
+        }
+    };
+
+    // Create a new FormData object to send the file
+    let combinedFormData = new FormData();
+    if (file) {
+        combinedFormData.append('fileUpload', file); // Add the file
+    }
+
+    xhr.open("POST", "UploadFile");
+    // Note: Do not set Content-Type header for FormData, the browser will automatically set it
+    xhr.send(combinedFormData);
+}
