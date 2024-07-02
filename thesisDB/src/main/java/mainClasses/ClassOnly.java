091515b2a002da20aee678e415dad8ef937f7b69
Makis <gerrylog@yahoo.com>
2024-07-02 20:35:47 +0300

    created basic main classes

---
+++ thesisDB/src/main/java/mainClasses/ClassOnly.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainClasses;
+
+/**
+ *
+ * @author gerry
+ */
+public class ClassOnly {
+    int pid;
+    String classFrom,classTo;
+    
+    public int getPid(){
+        return pid;
+    }
+    
+    public void setPid(int pid){
+        this.pid=pid;
+    }
+        
+    public String getClassFrom(){
+        return classFrom;
+    }
+    
+    public void setClassFrom(String classFrom){
+        this.classFrom=classFrom;
+    }
+    
+    public String getClassTo(){
+        return classTo;
+    }
+    
+    public void setClassTo(String classTo){
+        this.classTo=classTo;
+    }
+}
