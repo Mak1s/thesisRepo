79aa788076dcee469f9896aa49938509886d59b0
Makis <gerrylog@yahoo.com>
2024-07-29 15:46:53 +0300

    added login checker
    needs fixing

--- thesisProject/src/main/webapp/form.js
+++ thesisProject/src/main/webapp/form.js
@@ -44,11 +44,15 @@ function getUser() {
     console.log(xhr);
  xhr.onload = function () {
     if (xhr.readyState === 4 && xhr.status === 200) {
+        console.log("1");
         console.log(xhr.responseText); // Log the server response
-        try {
+        console.log("2");
+        console.log(xhr);
+         try {
             const responseData = JSON.parse(xhr.responseText);
             $('#ajaxContent').html("Successful Registration. Now please log in!<br> Your Data");
             $('#ajaxContent').append(createTableFromJSON(responseData));
+            console.log(responseData);
         } catch (e) {
             console.error("Failed to parse JSON response:", e);
             console.error("Response text:", xhr.responseText);
@@ -58,10 +62,39 @@ function getUser() {
         document.getElementById("ajaxContent").innerHTML = "Request failed. Returned status of " + xhr.status + "<br>";
     }
     };
+        console.log("json data->");
+        console.log(jsonData);
         xhr.open("POST", "GetUser");
-        console.log("1");
         xhr.setRequestHeader("Content-type", "application/json");
         xhr.send(jsonData);
-        console.log("2");
         
     }
+    
+    function login_post(){
+    
+    var data = $("#myForm").serialize();
+  //  $("#myForm")[0].reset();
+    console.log(data);
+    var xhttp = new XMLHttpRequest();
+    console.log(xhttp);
+    xhttp.onload = function() {
+        if (xhttp.readyState === 4 && xhttp.status === 200) {
+         //   document.getElementById("msg-login").style.color="rgb(0, 0, 153)";            
+            console.log("OK 2");
+            document.getElementById("msg").innerText="Connected";
+             document.getElementById("msg").innerText = xhttp.responseText;
+            window.location.href='./index.html';
+        }else if(xhttp.status === 401){
+            console.log("OK 3");
+         //   document.getElementById("msg-login").style.color="red";
+            document.getElementById("msg").innerText="Wrong Credentials";
+        } 
+    };
+        
+   
+
+    xhttp.open("POST","servlet_login");
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send(data);
+    
+}
