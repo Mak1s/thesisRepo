e5f22ac86375a6bdd9ec81ce71199077d36bdc19
Yannis Marketakis <yannismarketakis@gmail.com>
2024-09-19 15:22:57 +0300

    enhanced list methods to retrieve the namespace of resources

--- thesisProject/src/main/java/mainClasses/ModelLoader.java
+++ thesisProject/src/main/java/mainClasses/ModelLoader.java
@@ -9,7 +9,9 @@ import org.apache.jena.util.FileManager;
 import org.apache.jena.util.iterator.ExtendedIterator;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 /**
  * @author Yannis Marketakis (marketak 'at' ics 'dot' forth 'dot' gr)
@@ -29,6 +31,16 @@ public class ModelLoader {
             classSet.add(classIter.next().getURI());
         return classSet;
     }
+    
+    public Map<String,String> listClassesMap(){
+        ExtendedIterator<OntClass> classIter=this.model.listClasses();
+        Map<String,String> classMap=new TreeMap<>();
+        while(classIter.hasNext()){
+            OntClass nextClass=classIter.next();
+            classMap.put(nextClass.getLocalName(), nextClass.getNameSpace());
+        }
+        return classMap;
+    }
 
     public Collection<String> listProperties(){
         ExtendedIterator<OntProperty> propIter=this.model.listOntProperties();
@@ -37,10 +49,31 @@ public class ModelLoader {
             propertySet.add(propIter.next().getURI());
         return propertySet;
     }
+    
+    public Map<String,String> listPropertiesMap(){
+        ExtendedIterator<OntProperty> propIter=this.model.listOntProperties();
+        Map<String,String> propertyMap=new TreeMap<>();
+        while(propIter.hasNext()){
+            OntProperty nextProperty=propIter.next();
+            propertyMap.put(nextProperty.getLocalName(), nextProperty.getNameSpace());
+        }
+        return propertyMap;
+    }
 
     public static void main(String []args){
-        ModelLoader modelLoader=new ModelLoader("CIDOC_CRM_v7.1.1.rdfs");
-        modelLoader.listClasses().forEach( c -> System.out.println(c));
-        modelLoader.listProperties().forEach( p -> System.out.println(p));
+        ModelLoader modelLoader=new ModelLoader("CIDOC_CRM_v7.1.3.rdf");
+//        ModelLoader modelLoader=new ModelLoader("foaf.rdf");
+        
+//        modelLoader.listClasses().forEach( c -> System.out.println(c));
+//        modelLoader.listProperties().forEach( p -> System.out.println(p));
+
+        Map<String,String> classesMap=modelLoader.listClassesMap();
+        for(String className : classesMap.keySet()) {
+            System.out.println(className+"\t"+classesMap.get(className));
+        }
+        Map<String,String> propertiesMap=modelLoader.listPropertiesMap();
+        for(String propName : propertiesMap.keySet()) {
+            System.out.println(propName+"\t"+propertiesMap.get(propName));
+        }
     }
 }
